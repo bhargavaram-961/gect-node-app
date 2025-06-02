798798341f1f1197bd2623ed3d266697f40f2220
bhargavaram-961 <bhargavaram961@gmail.com>
2025-06-02 14:48:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import cors from 'cors'
+const app =express()
+app.listen(8080,()=>{console.log("Server Started")});
+app.use(cors)
+app.get("/",(req,res)=>{
+    return res.send("Good morning!")
+
+});
+app.get("/greet",(req,res)=>{
+    return res.send("Greetings fellow!")
+
+});
+app.get("/name",(req,res)=>{
+    return res.send("Bhargava Ram")
+
+});
+app.get("/weather",(req,res)=>{
+    return res.send("35")
+
+});
+app.get("/products",(req,res)=>{
+   const products=[
+    {name:"product 1",price:"45"},
+    {name:"product 2",price:"95"},
+    {name:"product 3",price:"50"},
+   ]
+   return res.json(products);
+
+});
+
