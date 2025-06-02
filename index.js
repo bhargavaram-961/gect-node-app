c96d9275385e9c574d95f27c1946451f4717a2ca
bhargavaram-961 <bhargavaram961@gmail.com>
2025-06-02 15:03:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express'
-import cors from 'cors'
-const app =express()
+const app = express()
 app.listen(8080,()=>{console.log("Server Started")});
 app.use(cors)
 app.get("/",(req,res)=>{
